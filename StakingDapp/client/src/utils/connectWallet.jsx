7929b4b2966af909c321c6f802c0f621c9f4a1ca
Sheetal Agarwal <106532662+sheetal123a@users.noreply.github.com>
2024-03-16 12:59:59 +0530

    Add files via upload

---
+++ StakingDapp/client/src/utils/connectWallet.jsx
@@ -0,0 +1,42 @@
+import {ethers,Contract} from "ethers";
+import stakingAbi from "../ABI/stakingAbi.json"
+import stakeTokenAbi from "../ABI/stakeTokenAbi.json";
+
+
+export const connectWallet = async()=>{
+    try{
+       let [signer,provider,stakingContract,stakeTokenContract,chainId]=[null,null,null,null,null];
+       if(window.ethereum===null){
+          throw new Error("Metamsk is not installed");
+       }
+       const accounts = await window.ethereum.request({
+        method:'eth_requestAccounts'
+       })
+
+       let chainIdHex= await window.ethereum.request({
+        method:'eth_chainId'
+       })
+       chainId= parseInt(chainIdHex,16)
+       
+       let selectedAccount =accounts[0];
+       if(!selectedAccount){
+        throw new Error("No ethereum accounts available")
+       } 
+
+       provider = new ethers.BrowserProvider(window.ethereum);
+       signer = await provider.getSigner();
+
+       const stakingContractAddress="0xbcdE9a393DB264349759dcEAfA2E94f58Ba12815"
+       const stakeTokenContractAddress="0xd0883806CdEd066Fa6F4E794Eb0497465766db34"
+
+       stakingContract= new Contract(stakingContractAddress,stakingAbi,signer);
+       stakeTokenContract=new Contract(stakeTokenContractAddress,stakeTokenAbi,signer);
+
+       return {provider,selectedAccount,stakeTokenContract,stakingContract,chainId}
+
+    }catch(error){
+        console.error(error);
+        throw error
+    }
+    
+}
